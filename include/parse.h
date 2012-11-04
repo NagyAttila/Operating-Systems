a9718cce21d744424b2cabc95c2d0ee59007c4e8
Attila Nagy <attilanagy85@gmail.com>
2012-11-04 17:16:07 +0100

    Indentation in source, list of tasks in doc.

--- include/parse.h
+++ include/parse.h
@@ -1,14 +1,14 @@
 typedef struct c {
-    char **pgmlist;
-    struct c *next;
+  char **pgmlist;
+  struct c *next;
 } Pgm;
 
 typedef struct node { 
-    Pgm  *pgm;
-    char *rstdin;
-    char *rstdout;
-    char *rstderr;
-    int bakground;
+  Pgm  *pgm;
+  char *rstdin;
+  char *rstdout;
+  char *rstderr;
+  int bakground;
 } Command;
 
 extern void init( void );
@@ -16,6 +16,4 @@ extern int parse ( char *, Command *);
 extern int nexttoken( char *, char **);
 extern int acmd( char *, Pgm **);
 extern int isidentifier( char * );
-
-
 
--- src/lsh.c
+++ src/lsh.c
@@ -23,42 +23,42 @@ int done = 0;
  */
 int main(void)
 {
-    Command cmd;
-    int n;
+  Command cmd;
+  int n;
 
-    while (! done) {
+  while (! done) {
 
-	char *line;
+    char *line;
 
-	line = readline("@ ");
+    line = readline("@ ");
 
-	if (!line)
-	{
-	    /* Encountered EOF at top level */
-	    done = 1;
-	}
-	else
-	{
-	    /*
-	     * Remove leading and trailing whitespace from the line
-	     * Then, if there is anything left, add it to the history list
-	     * and execute it.
-	     */
-	    stripwhite(line);
-
-	    if(*line)
-	    {
-		add_history(line);
-		/* execute it */
-		n = parse(line, &cmd);
-		PrintCommand(n, &cmd);
-	    }
-	}
-	
-	if(line)
-	    free(line);
+    if (!line)
+    {
+      /* Encountered EOF at top level */
+      done = 1;
     }
-    return 0;
+    else
+    {
+      /*
+       * Remove leading and trailing whitespace from the line
+       * Then, if there is anything left, add it to the history list
+       * and execute it.
+       */
+      stripwhite(line);
+
+      if(*line)
+      {
+        add_history(line);
+        /* execute it */
+        n = parse(line, &cmd);
+        PrintCommand(n, &cmd);
+      }
+    }
+
+    if(line)
+      free(line);
+  }
+  return 0;
 }
 
 /*
@@ -70,11 +70,11 @@ int main(void)
 void
 PrintCommand (int n, Command *cmd)
 {
-    printf("Parse returned %d:\n", n);
-    printf("   stdin : %s\n", cmd->rstdin  ? cmd->rstdin  : "<none>" );
-    printf("   stdout: %s\n", cmd->rstdout ? cmd->rstdout : "<none>" );
-    printf("   bg    : %s\n", cmd->bakground ? "yes" : "no");
-    PrintPgm(cmd->pgm);
+  printf("Parse returned %d:\n", n);
+  printf("   stdin : %s\n", cmd->rstdin  ? cmd->rstdin  : "<none>" );
+  printf("   stdout: %s\n", cmd->rstdout ? cmd->rstdout : "<none>" );
+  printf("   bg    : %s\n", cmd->bakground ? "yes" : "no");
+  PrintPgm(cmd->pgm);
 }
 
 /*
@@ -86,21 +86,21 @@ PrintCommand (int n, Command *cmd)
 void
 PrintPgm (Pgm *p)
 {
-    if (p == NULL)
-	return;
-    else
-    {
-	char **pl = p->pgmlist;
+  if (p == NULL)
+    return;
+  else
+  {
+    char **pl = p->pgmlist;
 
-	/* The list is in reversed order so print
-         * it reversed so get right :-)
-         */
-	PrintPgm(p->next);
-	printf("    [");
-        while (*pl)
-	    printf("%s ", *pl++);
-	printf("]\n");
-    }
+    /* The list is in reversed order so print
+     * it reversed so get right :-)
+     */
+    PrintPgm(p->next);
+    printf("    [");
+    while (*pl)
+      printf("%s ", *pl++);
+    printf("]\n");
+  }
 }
 
 /*
@@ -111,14 +111,14 @@ PrintPgm (Pgm *p)
 void
 stripwhite (char *string)
 {
-    register int i = 0;
-    while (whitespace( string[i] ))
-	i++;
-    if (i)
-	strcpy (string, string + i);
+  register int i = 0;
+  while (whitespace( string[i] ))
+    i++;
+  if (i)
+    strcpy (string, string + i);
 
-    i = strlen( string ) - 1;
-    while (i> 0 && whitespace (string[i]))
-	i--;
-    string [++i] = '\0';
+  i = strlen( string ) - 1;
+  while (i> 0 && whitespace (string[i]))
+    i--;
+  string [++i] = '\0';
 }
